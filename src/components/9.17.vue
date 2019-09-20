1a21656cc317c919c7199090d94a38548d05cb94
admin <1072458853@qq.com>
2019-09-20 10:16:27 +0800

    完成data格式化

--- src/components/9.17.vue
+++ src/components/9.17.vue
@@ -22,8 +22,8 @@ export default {
     
 
     methods:{
-        d3(){
-
+        d3(d){
+            console.log(JSON.parse(d));//JSON字符串转JSON对象
             
         },
 
@@ -33,11 +33,14 @@ export default {
                 this.a += 1
                 console.log(this.a);
 
+                let data =  response.data.split('}{').join('},{')//有结果输出，说明在前台可以取到数据
+                data = '['.concat(data).concat(']')//完成了数据的格式化，data是一个JSON数组
+
+                this.d3(data)
                 //开始轮询请求数据
                 setTimeout(()=>{
                     this.getData()
-                },100)//此处的时间就是每次间隔的时间，向后台发一次请求
-                // this.data =  response.data
+                },1000000)//此处的时间就是每次间隔的时间，向后台发一次请求
             })
         }
 
